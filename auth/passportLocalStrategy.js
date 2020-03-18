8cf656a567418851e1c93e07bed9ba6f211cbbea
Raj <rajsinghusa@yahoo.com>
2020-03-18 04:09:37 -0400

    corrected require for LocalStrategy

--- auth/passportLocalStrategy.js
+++ auth/passportLocalStrategy.js
@@ -1,5 +1,5 @@
 const passport = require('passport');
-const LocalStrategy = require('passport-local');
+const LocalStrategy = require('passport-local').Strategy;
 
 const init = require('./passportSessionConfig');
 const knex = require('../db/connection');
